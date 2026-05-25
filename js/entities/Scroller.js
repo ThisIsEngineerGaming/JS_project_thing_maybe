f3dc9aa808ba7266c2236385fe52559e43fb9516
Тимофей Терзиев <t.terziiev@gmail.com>
2026-05-25 22:21:37 +0300

    i be doing things

---
+++ js/entities/Scroller.js
@@ -0,0 +1,26 @@
+let track = document.getElementById("scrollTrack");
+let original = document.getElementById("original");
+
+while (track.offsetWidth < window.innerWidth * 2) {
+  let clone = original.cloneNode(true);
+  track.appendChild(clone);
+}
+
+let x = 0;
+let speed = 2;
+
+let singleWidth = original.offsetWidth;
+
+function animate() {
+  x -= speed;
+
+  if (Math.abs(x) >= singleWidth) {
+    x = 0;
+  }
+
+  track.style.transform = "translateX(" + x + "px)";
+
+  requestAnimationFrame(animate);
+}
+
+animate();
